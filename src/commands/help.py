d202410468f9a7248097bed8dfd946b652d8bb48
Yurb <yavurb@gmail.com>
2021-10-15 02:41:55 -0400

    ♻ ref(command): help command now sends an embed message

--- src/commands/help.py
+++ src/commands/help.py
@@ -1,14 +1,29 @@
+# description => Shows the bot's commands
+# options => []
+
 import os
+import json
+from discord import Embed, Colour
 
 async def help(message, args, client):
-  file_path = os.path.abspath(__file__)
-  dir_path = os.path.dirname(file_path)
-
-  filenames = os.listdir(dir_path)
+  command_path = os.path.abspath(__file__)
+  dir_path = os.path.dirname(command_path)
 
   commands = []
-  for file in filenames:
-    name = file.split('.')[0]
-    commands.append(name)
 
-  await message.channel.send('commands: \n'+'\n'.join(commands))
+  embed = Embed(title = 'Commands', colour = Colour.from_rgb(186, 85, 211))
+
+  with os.scandir(dir_path) as files:
+    for file in files:
+
+      file_path = os.path.join(dir_path, file)
+
+      with open(file_path, 'r') as opened_file:
+        description = opened_file.readline().split('=>')[1].strip()
+        options = opened_file.readline().split('=>')[1].strip()
+        options = f'option: {", ".join(json.loads(options))}' if options != '[]' else ''
+
+        name = file.name[:len(file.name) - 3]
+        embed.add_field(name = name, value = description, inline = False)
+
+  await message.channel.send(embed = embed)
